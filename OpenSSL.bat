f44173c88f6b22feac17da8c3aa0829ac5acfc95
BingNan Chou <cookie040667@gmail.com>
2019-01-29 15:35:40 +0800

    Update OpenSSL.bat

--- OpenSSL.bat
+++ OpenSSL.bat
@@ -1,4 +1,4 @@
-C:\
 cd C:\OpenSSL-Win32\bin
-start openssl pkcs12 -export -out  C:\sslforfree\certificate.pfx -inkey  C:\sslforfree\private.key -in  C:\sslforfree\certificate.crt -certfile  
+call openssl pkcs12 -export -out  C:\sslforfree\certificate.pfx -inkey  C:\sslforfree\private.key -in  C:\sslforfree\certificate.crt -certfile  
 pause
+ECHO finish
