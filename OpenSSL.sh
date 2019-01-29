304f51bce844bdd46db4fa935f5d3d2374376832
BingNan Chou <cookie040667@gmail.com>
2019-01-29 14:47:42 +0800

    Update OpenSSL.sh

--- OpenSSL.sh
+++ OpenSSL.sh
@@ -1,2 +1,5 @@
-cd C:\OpenSSL-Win32\bin
+#!/bin/bash
+PATH= /OpenSSL-Win32/bin
+export PATH
 openssl pkcs12 -export -out  C:\sslforfree\certificate.pfx -inkey  C:\sslforfree\private.key -in  C:\sslforfree\certificate.crt -certfile  C:\sslforfree\ca_bundle.crt
+exit 0
